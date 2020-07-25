2e669cb5ab22aa162baf149727f256010b78d4e2
Daniel Souza <daniellrs3@hotmail.com>
2020-07-25 20:45:48 -0300

    voltando api baseURL para staging

--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-    // baseURL: "https://staging-mealtime-api.herokuapp.com/"
-    baseURL: "http://localhost:3333/"
+    baseURL: "https://staging-mealtime-api.herokuapp.com/"
 })
 
 export default api;
